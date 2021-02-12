841a3e0c0db164c84a1009b218742fbf3479ed73
dimontejulia <dimontejulia@gmail.com>
2021-02-12 11:42:58 -0500

    build grid and tetrominos

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,17 +1,21 @@
 import React from "react";
+
+import { createStage } from "../gameHelpers";
+
+// Components
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 
-const Tetris = ({ callback }) => {
+const Tetris = () => {
   return (
     <div>
-      <Stage />
+      <Stage stage={createStage()} />
       <aside>
         <div>
           <Display text="Score" />
           <Display text="Rows" />
-          <Display text="Level 1" />
+          <Display text="Level" />
         </div>
         <StartButton />
       </aside>
