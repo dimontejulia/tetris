6c4111b7200b94be80e76b8c7126349fecb9d92a
dimontejulia <dimontejulia@gmail.com>
2021-02-12 11:51:44 -0500

    styling cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
-const Cell = ({ type }) => <div>cell</div>;
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS["L"].color}>
+    cell
+  </StyledCell>
+);
 
 export default Cell;
