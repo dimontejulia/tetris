6fbcb3e10d218bb618a6110b676992d95d6afc56
dimontejulia <dimontejulia@gmail.com>
2021-02-16 16:29:02 -0500

    colliding function & game over when reach top

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -5,3 +5,24 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     new Array(STAGE_WIDTH).fill([0, "clear"])
   );
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  for (let y = 0; y < player.tetromino.length; y += 1) {
+    for (let x = 0; x < player.tetromino[0].length; x += 1) {
+      //check we're on a tetromino cell
+      if (player.tetromino[y][x] !== 0) {
+        //check we're within height bounds (y)
+        if (
+          !stage[y + player.pos.y + moveY] ||
+          //check we're within width bounds (x)
+          !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+          //check cell is not set to clear (not colliding with something)
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+            "clear"
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+};
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,9 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createStage } from "../gameHelpers";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   //props destructure
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // clear the stage contents
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      // draw the generated tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          //valid cell of tetromino
+          if (value !== 0) {
+            //coordinates of stage
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              // merged: we know it should keep it in the stage
+              // otherwise set to clear
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+      if (player.collided) {
+        resetPlayer();
+      }
+      return newStage;
+    };
+    setStage((prev) => updateStage(prev));
+  }, [player, resetPlayer]);
 
   return [stage, setStage];
 };
