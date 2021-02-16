a45e002cf33f9ce658e6bd9ada204a8101d1e6e2
dimontejulia <dimontejulia@gmail.com>
2021-02-16 17:20:37 -0500

    tetris auto fall

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from "react";
 
 import { TETROMINOS, randomTetromino } from "../tetrominos";
-import { STAGE_WIDTH } from "../gameHelpers";
+import { checkCollision, STAGE_WIDTH } from "../gameHelpers";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -24,6 +24,20 @@ export const usePlayer = () => {
     //deep copy of the player state
     const clonedPlayer = JSON.parse(JSON.stringify(player));
     clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    //make sure you can't rotate outside of game bounds
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        //can't rotate, rotate back
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
 
     setPlayer(clonedPlayer);
   };
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,8 +4,23 @@ import { createStage } from "../gameHelpers";
 export const useStage = (player, resetPlayer) => {
   //props destructure
   const [stage, setStage] = useState(createStage());
+  const [rowCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = (newStage) =>
+      newStage.reduce((accumulator, row) => {
+        //filled up a complete row
+        if (row.findIndex((cell) => cell[0] === 0) === -1) {
+          setRowsCleared((prev) => prev + 1);
+          accumulator.unshift(new Array(newStage[0].length.fill([0, "clear"])));
+          return accumulator;
+        }
+        accumulator.push(row);
+        return accumulator;
+      }, []);
+
     const updateStage = (prevStage) => {
       // clear the stage contents
       const newStage = prevStage.map((row) =>
@@ -29,11 +44,12 @@ export const useStage = (player, resetPlayer) => {
       });
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
       return newStage;
     };
     setStage((prev) => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowCleared];
 };
